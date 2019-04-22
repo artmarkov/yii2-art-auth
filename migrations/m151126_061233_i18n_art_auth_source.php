ecc99cd648818f3e4016da0deb11416abbe121e8
artmarkov <artmarkov@mail.ru>
2019-04-22 09:04:08 +0300

    Update m151126_061233_i18n_art_auth_source.php

--- migrations/m151126_061233_i18n_art_auth_source.php
+++ migrations/m151126_061233_i18n_art_auth_source.php
@@ -63,7 +63,7 @@ class m151126_061233_i18n_art_auth_source extends SourceMessagesMigration
             'Update Password' => 1,
             'User Profile' => 1,
             "User with the same email as in {client} account already exists but isn't linked to it. Login using email first to link it." => 1,
-            'The username should contain only Latin letters, numbers and the following characters: "-" and "_".' => 1,
+            'The username should contain only Latin letters, numbers and the following characters: "_".' => 1,
             'Username contains not allowed characters or words.' => 1,
             'Wrong password' => 1,
             'You could not login from this IP' => 1,
